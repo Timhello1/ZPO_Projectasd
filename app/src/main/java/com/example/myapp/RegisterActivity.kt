276d8f0ae8dfe8fcdd5949f9702dd0f473e948b4
Timhello1 <121805566+Timhello1@users.noreply.github.com>
2023-04-28 07:42:31 +0200

    Podział użytkowników zalążek

--- app/src/main/java/com/example/myapp/RegisterActivity.kt
+++ app/src/main/java/com/example/myapp/RegisterActivity.kt
@@ -1,18 +1,11 @@
 package com.example.myapp
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import androidx.navigation.fragment.findNavController
-import com.example.myapp.databinding.FragmentRegisterBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -31,11 +24,13 @@ class RegisterActivity : BaseActivity() {
     private var inputPassword: EditText? = null
     private var inputRepPass: EditText? = null
     private var goBackButton: Button? = null
+    private var adminPass: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_register)
 
+        adminPass = findViewById(R.id.adminPass)
         registerButton = findViewById(R.id.buttonRegister2)
         inputEmail = findViewById(R.id.inputEmail)
         inputName = findViewById(R.id.inputName)
@@ -43,9 +38,11 @@ class RegisterActivity : BaseActivity() {
         inputRepPass = findViewById(R.id.inputPassword2Repeat)
         goBackButton = findViewById(R.id.buttonGoBack)
 
+
         registerButton?.setOnClickListener {
             //validateRegisterDetails()
             registerUser()
+
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
